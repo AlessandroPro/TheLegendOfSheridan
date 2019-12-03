a18f5004228d8a47968990f4c4be7a801924ed70
Abhishektippi <abhishekreddycool@gmail.com>
2019-12-03 16:37:21 -0500

    no message

---
+++ Assets/Scripts/Enemies/AIMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMovement : MonoBehaviour
+{
+    public GameObject player;
+    private NavMeshAgent nav;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nav.SetDestination(player.transform.position);
+    }
+}
